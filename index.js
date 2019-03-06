93d5bdbf13ddfbf4aca1918a409977aefa240dde
Lcfvs <michael.rouges@gmail.com>
2019-03-06 05:31:05 +0100

    Avoids to extract dist files if watching

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = function ParcelPreDistPlugin (bundler) {
   const dist = bundler.options.publicURL
   const root = bundler.options.rootDir
 
-  if (dist !== root) {
+  if (!bundler.options.watch && dist !== root) {
     bundler.on('bundled', listener(bundler, root, dist))
   }
 }
